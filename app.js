31fd1e1085e4def687728f3fb178d82759cb5c93
BouzidYoussef <bouzidyoussef600@yahoo.com>
2021-07-28 10:07:22 +0100

    input success verification

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2= document.getElementById('password2');
+    //Show input error message
+    function showError(input, message){
+        const formControl = input.parentElement;
+        formControl.className = 'form-control error';
+        const small = formControl.querySelector('small');
+        small.innerText = message
+    }
 
+    //S how success
+    function showSuccess(input){
+        const formControl = input.parentElement;
+        formControl.className = 'form-control success';
+    }
+
+    //Check email is valid
+    function isValidEmail(email){
+        
+    }
+
+    //Event Listeners
     form.addEventListener('submit', function(e){
         e.preventDefault();
         if(username.value === ''){
@@ -11,6 +30,20 @@ const password2= document.getElementById('password2');
         }else {
             showSuccess(username);
         }
+        if(email.value === ''){
+            showError('Email is required')
+        }else {
+            showSuccess(email);
+        }
+        if(password.value === ''){
+            showError('Password is required')
+        }else {
+            showSuccess(password);
+        }
+        if(password2.value === ''){
+            showError('Password confirmation is required')
+        }else {
+            showSuccess(password2);
+        }
 
-        console.log(username.value);
-    })
+     })
